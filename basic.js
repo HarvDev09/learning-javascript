9556b8a2fcda04c0520427adb14f6b906165a5ca
HarvDev <perezharvey578@gmail.com>
2025-11-27 05:16:11 -0400

    fin del primer capitulo y cambio de nombre de concat.js a basic.js

---
+++ basic.js
@@ -0,0 +1,50 @@
+let name = "harvey";
+let weakness = "have ego";
+let space = " ";
+
+console.log(name.concat(space,weakness));
+
+let frase = `hola soy "harv"`;
+
+console.log(frase);
+
+//ejercicios de operadores
+
+
+let number = "30";
+let number2 = 30;
+
+console.log(number === number2);
+
+let num = 12;
+let num2 =24;
+let num3 = 25;
+let num4 = 92;
+let num5 = 91;
+
+let result = (num < num2 || num2 < num3) && (!num && num5 != num3);
+
+console.log(result); // false
+
+let op = (num5 > num2 && num4 < num3) || (!(num === num2) || num3 != num3);
+
+console.log(op); //true
+
+//condicionales
+
+
+let tengoDinero = false;
+
+if (tengoDinero == true){
+
+console.log("Puedes viajar.");
+
+}
+
+else {
+
+console.log("No puedes viajar.")
+
+
+}
+//fin del capitulo uno.
